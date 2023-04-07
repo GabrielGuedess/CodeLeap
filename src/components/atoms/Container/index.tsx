10bf5fdd6457a4034ca528d87f78ac6462bb3cc9
Gabriel Guedes <gabrielrguedess@gmail.com>
2023-04-07 20:12:24 -0300

    fix: add new breakpoint in container

--- src/components/atoms/Container/index.tsx
+++ src/components/atoms/Container/index.tsx
@@ -1,13 +1,13 @@
 import styled, { css } from 'styled-components';
 
-import media from 'styled-media-query';
+import { customMedia } from 'utils/media/customMedia';
 
 export const Container = styled.div`
   ${({ theme }) => css`
     max-width: ${theme.grid.container};
     margin: 0 1.6rem;
 
-    ${media.greaterThan('large')`
+    ${customMedia.greaterThan('normal')`
       margin-left: auto;
       margin-right: auto;
     `}
---
+++ src/utils/media/customMedia.ts
@@ -0,0 +1,9 @@
+import { generateMedia } from 'styled-media-query';
+
+export const customMedia = generateMedia({
+  large: '1170px',
+  big: '1024px',
+  normal: '832px',
+  medium: '768px',
+  small: '450px',
+});
